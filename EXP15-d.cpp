cf454d6aecd68bf072271c4e0bf70eda4f4aeb3b
imdhairyathakkar-spec <imdhairyathakkar@gmail.com>
2025-10-04 10:07:44 +0530

    Create EXP15-d.cpp

---
+++ EXP15-d.cpp
@@ -0,0 +1,27 @@
+//Dhairya Thakkar
+//24070123037
+//A2
+#include <iostream>
+#include <string.h>
+
+void print_rev(int i){
+    if(i>0) 
+    {
+        std::cout<<(i%10);
+        print_rev(i/10); 
+    }
+}
+    
+int main(){
+    int i;
+    std::cout<<"Enter the number: ";
+    std::cin>>i;
+    std::cout<<"The reverse of the number is: ";
+    print_rev(i); 
+    return 0;
+}
+
+/*Output:
+Enter the number: 1234567890
+The reverse of the number is: 0987654321
+*/
